5b4ce20938fa62381666522730e089e8259b0b0a
Charles Thorn <thornct3@gmail.com>
2024-11-11 23:37:18 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,107 @@
+export const tweetsData = [   
+    {
+        handle: `@TrollBot66756542 💎`,
+        profilePic: `images/troll.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
+            Guaranteed return on investment. HMU DMs open!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@BenGotDetroit`,
+        profilePic: `images/breaking bad kon.jpg`,
+        likes: `8439`,
+        retweets: 746,
+        tweetText: `I severely regret making that TikTok account...`,
+        replies: [
+                  {
+                handle: `@JoePlaysMinecraft ✅`,
+                profilePic: `images/biden.jpg`,
+                tweetText: `We know what you are buddy.`,
+            },
+                  {
+                handle: `@IsThatFrost`,
+                profilePic: `images/jax_mk11.png`,
+                tweetText: `I kinda want it back`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ez-c0f5-9g9g-9c4b-77835tgs2',
+    },
+    {
+        handle: `@Elon ✅`,
+        profilePic: `images/musk.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+        replies: [
+                  {
+                handle: `@TomCruise ✅`,
+                profilePic: `images/tcruise.png`,
+                tweetText: `Yes! Sign me up! 😎🛩`,
+            },
+                  {
+                handle: `@ChuckNorris ✅`,
+                profilePic: `images/chucknorris.jpeg`,
+                tweetText: `I went last year😴`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@NoobCoder12`,
+        profilePic: `images/flower.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
+            },
+            {
+                handle: `@YummyCoder64`,
+                profilePic: `images/love.png`,
+                tweetText: `You are wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+    {
+        handle: `@313ljay`,
+        profilePic: `images/velma 2.jpg`,
+        likes: `27802`,
+        retweets: `1156`,
+        tweetText: `Luka Doncic may be the greatest player in the NBA`,
+        replies: [
+                  {
+                handle: `@HonorRollStudent99`,
+                profilePic: `images/headshot3.jpg`,
+                tweetText: `He's definetely on a heater rn 🔥`,
+            },
+                  {
+                handle: `@YesKing ✅`,
+                profilePic: `images/bron thanos.jpg`,
+                tweetText: `He's soakin it up with the 3 balls`,
+            },
+                  {
+                handle: `@BenGotDetroit`,
+                profilePic: `images/breaking bad kon.jpg`,
+                tweetText: `Not may be, HE IS the best player in the league rn`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ed-c0f5-9g9g-9c4b-77835tgs2',
+    },
+]
